c648f92955aad6fde86b3d90312d1c66fa936964
JesusMarcano <jesusmarcanu24@gmail.com>
2025-06-21 11:47:52 -0500

    feat(reports): adding api rest forr reports

---
+++ project-cointrack/src/main/java/com/cibertec/models/Usuario.java
@@ -0,0 +1,65 @@
+package com.cibertec.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "USUARIOS")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	private String username;
+	private String rol;
+	private String password;
+
+	@Transient
+	private String confirmPassword;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
